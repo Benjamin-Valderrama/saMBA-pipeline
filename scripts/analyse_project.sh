36ab2967b22a8463395e529a75f2138b9de13e7b
Benjamin Valderrama <bvalderramabobadilla@gmail.com>
2025-01-31 11:49:28 +0000

    clean up and add flag to reference db

--- scripts/analyse_project.sh
+++ scripts/analyse_project.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Workflow arguments: Default values
+# This script was recycled, so although it can be used to download data, that option is not used here
+
+# Required arguments: Default values
 study_folder=""
-accession_number=""
+refdb=""
 
+# Workflow arguments: Default values
 run_all=false
-run_download=false
 run_dada2=false
-
 
 # Set path to folder with the scripts used in saMBA
 SCRIPTS_FOLDER="$(dirname "$(realpath "$0")")" # Folder with all scripts
@@ -17,21 +18,18 @@ ANALYSIS_SCRIPTS="${SCRIPTS_FOLDER}/analysis" # Folder with scripts used in the 
 # Function to display script usage
 function display_usage() {
     echo ""
-    echo "Usage: $0 -s|--study_folder STUDY_FOLDER [-a|--accession_number ACCESSION_NUMBER] [-r|--run_all]"
-    echo "	[--run_download] [--dada2] [-h|--help]"
-    echo ""
-    echo "Optional arguments:"
-    echo "  -h, --help               Display this help message."
+    echo "Usage: $0 -s|--study_folder STUDY_FOLDER [-r|--run_all] [--refdb path/to/database]"
     echo ""
     echo "Required arguemnts:"
     echo "  -s, --study_folder       Specify the name for the study included in this meta-analysis. (Required)"
-    echo "  -a, --accession_number   Specify the accession number of the raw data at the ENA (Only used if --run_all or --run_download are provided)."
+    echo "  --refdb                  Path to the taxonomy reference database"
     echo ""
     echo "Workflow arguments:"
     echo "  -r, --run_all            Run all steps of the workflow."
-    echo "  --run_download           Run data download [uses fastq-dl]."
     echo "  --run_dada2              Run reads quality check and alignment [uses DADA2 in R]."
     echo ""
+    echo "Optional arguments:"
+    echo "  -h, --help               Display this help message."
     exit 1
 }
 
@@ -45,17 +43,13 @@ while [[ $# -gt 0 ]]; do
             shift
             shift
             ;;
+        --refdb)
+            refdb="$2"
+            shift
+            shift
+            ;;
         -r|--run_all)
             run_all=true
-            shift
-            ;;
-        -a|--accession_number)
-            accession_number="$2"
-            shift
-            shift
-            ;;
-        --run_download)
-            run_download=true
             shift
             ;;
         --run_dada2)
@@ -83,12 +77,12 @@ if [ "$run_all" = true ] || [ "$run_dada2" = true ]; then
     if [ "$library_layout" = paired_end ]; then
         # run dada2 in pair end mode
         echo "PROGRESS -- Performing paired end taxonomic profiling with DADA2."
-        Rscript ${ANALYSIS_SCRIPTS}/PE_dada2.R ${study_folder} > ${study_folder}/nohups/dada2.log
+        Rscript ${ANALYSIS_SCRIPTS}/PE_dada2.R ${study_folder} ${refdb} > ${study_folder}/nohups/dada2.log
 
     elif [ "$library_layout" = single_end ]; then
         # run dada2 in single end mode
         echo "PROGRESS -- Performing single end taxonomic profiling with DADA2."
-        Rscript ${ANALYSIS_SCRIPTS}/SE_dada2.R ${study_folder} > ${study_folder}/nohups/dada2.log
+        Rscript ${ANALYSIS_SCRIPTS}/SE_dada2.R ${study_folder} ${refdb} > ${study_folder}/nohups/dada2.log
 
     else
 	echo "[ERROR] -- Library layout undetermined. Project can't be included."
@@ -115,7 +109,7 @@ elif [[ $library_layout == "paired_end" ]] && [[ $quality_check != "PASSED" ]]; 
     library_layout="single_end"
 
     echo "PROGRESS -- Re-analysing project as : ${library_layout}"
-    Rscript ${ANALYSIS_SCRIPTS}/SE_dada2.R ${study_folder} > ${study_folder}/nohups/dada2.log
+    Rscript ${ANALYSIS_SCRIPTS}/SE_dada2.R ${study_folder} ${refdb} > ${study_folder}/nohups/dada2.log
 
     # Check the results of the single end re run
     quality_check_rerun=$( Rscript ${ANALYSIS_SCRIPTS}/quality_check_dada2.R ${study_folder} ${library_layout})
