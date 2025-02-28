caf066fa972d92c64742813293961884f24e1df7
Benjamin Valderrama <bvalderramabobadilla@gmail.com>
2025-02-28 17:48:04 +0000

    consolidate ASV- and genus-level count tables

--- scripts/consolidation/consolidate_archive_count_table.R
+++ scripts/consolidation/consolidate_archive_count_table.R
@@ -1,24 +1,69 @@
 suppressMessages(library(tidyverse))
 suppressMessages(library(purrr))
 
+###################
+#### VARIABLES ####
+###################
 
-analysed_projects_folder <- commandArgs(trailingOnly = TRUE)[1]
+projects_folder <- commandArgs(trailingOnly = TRUE)[1]
+#projects_folder
 
-# Path to the count tables of every project aggregated up to the genus level
-paths_to_genus_lvl_clean_count_tables <- list.files(path = analysed_projects_folder, 
-	   pattern = "clean_count_table_genus.tsv", 
+# Paths to ASV-level count tables from each project
+paths_to_asv_ct <- list.files(path = projects_folder,
+           pattern = "ASV_count_table.tsv",
 	   recursive = TRUE)
+# Get only the count tables in the outputs folder
+paths_to_asv_ct <- paths_to_asv_ct[grepl(x = paths_to_asv_ct, pattern = "/outputs/")]
+paths_to_asv_ct <- paste(projects_folder, paths_to_asv_ct, sep = "/")
 
+# Paths to Genus-level count tables from each project
+paths_to_genus_ct <- list.files(path = projects_folder,
+           pattern = "genus_count_table.tsv",
+	   recursive = TRUE)
+# Get only the count tables in the outputs folder
+paths_to_genus_ct <- paths_to_genus_ct[grepl(x = paths_to_genus_ct, pattern = "/outputs/")]
+paths_to_genus_ct <- paste(projects_folder, paths_to_genus_ct, sep = "/")
 
-unwanted_taxonomies <- "Eukaryota;NA;NA;NA;NA;NA|Bacteria;NA;NA;NA;NA;NA|NA;NA;NA;NA;NA;NA"
+###################
+#### FUNCTIONS ####
+###################
 
-# read the count tables from the list above, merge them and output the consolidated table
-purrr::map(.x = paste0(analysed_projects_folder, paths_to_genus_lvl_clean_count_tables),
-	   .f = ~ read_tsv(file = .x, show_col_types = FALSE)) %>%
-	# remove eukaryotes and reads that couldn't be identified at the phylum level
-        purrr::map(.x = .,
-		   .f = ~ filter(.data = ., !grepl(x = full_taxonomy, pattern = unwanted_taxonomies))) %>%
-	purrr::reduce(full_join, by = "full_taxonomy") %>%
-	mutate(across(.cols = everything(), .fns = ~ifelse(is.na(.x), yes = 0, no = .x))) %>%
-	write_tsv(x = ., 
-		  file = paste0(analysed_projects_folder, "consolidated/archive_count_table.tsv"))
+consolidate_table <- function(vector_with_paths, level){
+
+    # level is either:
+    # 'full_taxonomy', for genus-level
+    # 'sequence', for ASV-level
+
+    # Read the count tables
+    count_tables_list <- vector_with_paths %>%
+	map(.x = .,
+	    .f = ~ read_tsv(file = .x, show_col_types = FALSE))
+
+    if(level == "sequence"){
+	count_tables_list <- count_tables_list %>%
+	    map(.x = .,
+		.f = ~ select(.data = .x, !full_taxonomy)
+		)
+    }
+
+    consolidated_table <- count_tables_list %>%    
+	# Join all count tables by 'level'
+        reduce(.f = full_join, by = level) %>%
+	# Convert NAs (absent taxonomies) to 0s
+	mutate(across(.cols = everything(), 
+		      .fns = ~ifelse(is.na(.x), yes = 0, no = .x))) %>%
+	relocate(matches(level))
+
+    # return
+    consolidated_table
+}
+
+# Consolidate ASV-level count table and export
+consolidated_asv_ct <- consolidate_table(paths_to_asv_ct, "sequence")
+write_tsv(x = consolidated_asv_ct, 
+	  file = paste0(projects_folder, "consolidated/archive_asv_count_table.tsv"))
+
+# Consolidate genus-level count table and export
+consolidated_genus_ct <- consolidate_table(paths_to_genus_ct, "full_taxonomy")
+write_tsv(x = consolidated_genus_ct,
+          file = paste0(projects_folder, "consolidated/archive_genus_count_table.tsv"))
