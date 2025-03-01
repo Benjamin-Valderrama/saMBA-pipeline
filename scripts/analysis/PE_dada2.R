39260251669ff698b3110d745665b9d0e857be9b
Benjamin Valderrama <bvalderramabobadilla@gmail.com>
2025-03-01 20:55:51 +0000

    remove white space before DNA sequence in fasta file

--- scripts/analysis/PE_dada2.R
+++ scripts/analysis/PE_dada2.R
@@ -176,9 +176,9 @@ write.table(x = clean_count_table_tax,
 
 # ASVs fasta file
 write.table(
-	x = paste(">ASV", 1:nrow(clean_count_table_tax), clean_count_table_tax$full_taxonomy, "\n",
-		  clean_count_table_tax$sequence
-		  ),
+	x = paste0(">ASV",1:nrow(clean_count_table_tax)," ", clean_count_table_tax$full_taxonomy, "\n",
+		   clean_count_table_tax$sequence
+		   ),
 	file = paste0(study_folder, "/01.dada2/ASVs.fa"),
 	quote = FALSE, row.names = FALSE, col.names = FALSE
 	)
--- scripts/analysis/SE_dada2.R
+++ scripts/analysis/SE_dada2.R
@@ -181,9 +181,9 @@ write.table(x = clean_count_table_tax,
 
 # ASVs fasta file
 write.table(
-        x = paste(">ASV", 1:nrow(clean_count_table_tax), clean_count_table_tax$full_taxonomy, "\n",
-                  clean_count_table_tax$sequence
-		  ),
+        x = paste0(">ASV", 1:nrow(clean_count_table_tax)," ", clean_count_table_tax$full_taxonomy, "\n",
+                   clean_count_table_tax$sequence
+		   ),
         file = paste0(study_folder, "/01.dada2/SE_ASVs.fa"),
         quote = FALSE, row.names = FALSE, col.names = FALSE
         )
