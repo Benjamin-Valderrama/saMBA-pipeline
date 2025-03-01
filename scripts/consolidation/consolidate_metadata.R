87b9626a7a9c254a48850558f2faacc9949de4f6
Benjamin Valderrama <bvalderramabobadilla@gmail.com>
2025-03-01 18:02:42 +0000

    tidy up & ENA -> INSDC

--- scripts/consolidation/consolidate_metadata.R
+++ scripts/consolidation/consolidate_metadata.R
@@ -1,20 +1,31 @@
 suppressMessages(library(tidyverse))
 
-folder_with_analysed_projects <- commandArgs(trailingOnly = TRUE)[1]
+projects_folder <- commandArgs(trailingOnly = TRUE)[1]
+
+###############
+## FUNCTIONS ##
+###############
+log <- function(path){
+    cat(paste("Analysing:", path, "\n"))
+}
 
 # get consolidated file reports and track_reads
-file_reports <- read_tsv(file = paste0(folder_with_analysed_projects, "consolidated/ena_file_reports.tsv"), show_col_type = FALSE)
-track_reads <- read_tsv(file = paste0(folder_with_analysed_projects, "consolidated/track_reads.tsv"), show_col_type = FALSE)
+file_reports_path <- paste0(projects_folder, "consolidated/insdc_file_reports.tsv")
+track_reads_path <- paste0(projects_folder, "consolidated/track_reads.tsv")
 
+# read the consolidated files
+log(file_reports_path)
+file_reports <- read_tsv(file = file_reports_path, show_col_type = FALSE)
 
-# inner join is used because track_reads only uses the info for samples included in final compendium,
-# whereas file reports has information for all the samples downloaded, even if they are low quality.
+log(track_reads_path)
+track_reads <- read_tsv(file = track_reads_path, show_col_type = FALSE)
 
-#print(paste("FR", dim(file_reports)))
-#print(paste("TR", dim(track_reads)))
-
-merged <- inner_join(x = file_reports,,
+# While track_reads only uses the info for samples included in final compendium,
+# file reports has information for all the samples in the bioproject 
+# (including those not downloaded). Thus, thanks to inner_join we can get the 
+# info in INSDC for the analysed samples.
+merged <- inner_join(x = file_reports,
 		     y = track_reads,
 		     by = "run_accession")
 
-write_tsv(x = merged, file = paste0(folder_with_analysed_projects, "consolidated/metadata.tsv"))
+write_tsv(x = merged, file = paste0(projects_folder, "consolidated/metadata.tsv"))
