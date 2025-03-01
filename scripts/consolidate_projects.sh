ae88f631a3b8670e01c46e7d6244ae963f3d07bf
Benjamin Valderrama <bvalderramabobadilla@gmail.com>
2025-03-01 18:05:58 +0000

    reduce redundancy in names

--- scripts/consolidate_projects.sh
+++ scripts/consolidate_projects.sh
@@ -2,6 +2,7 @@
 
 # read in the output folder
 output=$1
+output="${output%/}" # trail last '/' if present
 mkdir ${output}/consolidated
 
 # Set path to folder with the scripts used in saMBA
@@ -9,31 +10,30 @@ SCRIPTS_FOLDER="$(dirname "$(realpath "$0")")" # Folder with all scripts
 CONSOLIDATION_SCRIPTS="${SCRIPTS_FOLDER}/consolidation" # Folder with scripts used in the analysis of one project
 
 
-# use R to consolidate the files generated for each project included
-
-# clean count tables of each project are consolidated into one table
+# Consolidate ASV- and Genus-level count tables
 echo "PROGRESS -- consolidating count tables"
-Rscript ${CONSOLIDATION_SCRIPTS}/consolidate_archive_count_table.R $output/ > $output/logs/count_tables_consolidation.log 2>&1
+Rscript ${CONSOLIDATION_SCRIPTS}/archive_count_table.R $output/ > $output/logs/count_tables_consolidation.log 2>&1
 last_pid=$!
 wait $last_pid
 
-# ENA file reports of each project are consolidated into one table
-echo "PROGRESS -- consolidating ENA file reports"
-Rscript ${CONSOLIDATION_SCRIPTS}/consolidate_ena_filereports.R $output/ > $output/logs/ena_filereports_consolidation.log 2>&1
+# Consolidate ASVs fasta file from ASV-level count table
+#echo "PROGRESS -- consolidating ASVs fasta file"
+
+# INSDC metadatas are consolidated
+echo "PROGRESS -- consolidating INSDC file reports"
+Rscript ${CONSOLIDATION_SCRIPTS}/insdc_filereports.R $output/ > $output/logs/insdc_filereports_consolidation.log 2>&1
 last_pid=$!
 wait $last_pid
 
 # files tracking the reads of each sample through each step of the DADA2 pipeline are consolidated
 echo "PROGRESS -- consolidating 'track_reads_through_pipeline' files"
-Rscript ${CONSOLIDATION_SCRIPTS}/consolidate_track_reads.R $output/ > $output/logs/track_reads_consolidation.log 2>&1
+Rscript ${CONSOLIDATION_SCRIPTS}/track_reads.R $output/ > $output/logs/track_reads_consolidation.log 2>&1
 last_pid=$!
 wait $last_pid
 
-# we merge file reports and track_reads files. This removes all the samples in the ENA metadata
-# that weren't included in the actual analysis.
-echo "PROGRESS -- merge ENA file reports and track_reads_through_pipeline"
-Rscript ${CONSOLIDATION_SCRIPTS}/consolidate_metadata.R $output/ > $output/logs/metadata_consolidation.log 2>&1
+# join consolidated file reports and track_reads files.
+# This removes all the samples in the ENA metadata that weren't included in the actual analysis.
+echo "PROGRESS -- merge INSDC file reports and track_reads_through_pipeline"
+Rscript ${CONSOLIDATION_SCRIPTS}/metadata.R $output/ > $output/logs/metadata_consolidation.log 2>&1
 last_pid=$!
 wait $last_pid
-
-# we can remove the consolidated filereports and track_read files
